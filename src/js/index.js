1587e9095a147cbeb570f526abcbcb7475c2f851
Samuel Burke <Sam Burke>
2021-01-29 13:39:27 +0100

    pacman mounting working

--- src/js/index.js
+++ src/js/index.js
@@ -4,31 +4,42 @@
 console.log('it works!');
 const pacMan = document.querySelector('.entity--pac')
 
-
 let xpos = 0;
 let ypos = 0;
 const TILE_SIZE = 85;
+let direction = 'right'
 
 class Pacman {
   constructor(xPos, yPos, mouth, pacMan) {
-    this.pacMan = pacMan;
+    this.pacMan = pacMan
     this.xPos = xPos;
     this.yPos = yPos;
     this.mouth = mouth;
   }
-  // moveRight() {
-  //   this.xPos++
-  // }
+  render() {
+    const pacElm = document.createElement('div');
+    pacElm.innerHTML = `
+    <div class="entity entity--pac pacboy-active-light"></div>
+    `
+    return pacElm;
 
-  // moveLeft() {
-  //   this.xPos--
-  // }
+    //adding event listeners
+    
 
-  move() {
+  }
+  mount(parent) { 
+    this.element = this.render();
+    parent.appendChild(this.element);
+  }
+  reset() { 
     this.pacMan.classList.remove('pacman--right')
     this.pacMan.classList.remove('pacman--left')
     this.pacMan.classList.remove('pacman--down')
     this.pacMan.classList.remove('pacman--up')
+  }
+
+
+  move() {
     if (direction === 'right') {
       this.xPos++
       this.pacMan.classList.add('pacman--right')
@@ -45,7 +56,6 @@ class Pacman {
       console.log('nothing')
     }
   }
-
   update() {
     this.pacMan.classList.toggle('pacman--closed')
     if (direction === 'right' || direction === 'left') {
@@ -55,35 +65,18 @@ class Pacman {
     } else {
       console.log('notup')
     }
-    // this.mouth();
-    // console.log(mouth);
   }
-
-  // mouth() {
-  //   this.pacman.classList.toggle('pacman--closed')
-  // }
-
-  // mouth() { 
-  //   if (this.mouth === true) { 
-  //     this.mouth = false;
-  //     // this.pacman.classList.toggle('pacman--closed')
-  //   } else { 
-  //     this.mouth = true
-  //   }
-  // }
 }
 
 const pacman = new Pacman(0, 0, true, pacMan);
+pacman.mount(document.querySelector("#app"));
 
-
-
-// Toggle direction
-let direction = 'right'
 // Right arrow
 document.addEventListener('keydown', (e) => {
   if (e.keyCode === 39) {
     console.log(e)
     direction = 'right'
+    pacman.reset();
     pacman.move();
     pacman.update();
   } else {
