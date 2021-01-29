450a292dab989b4461637bc0e6b92b517e815cc3
Ma-Rah <martin.lars.rahm@gmail.com>
2021-01-29 12:26:55 +0100

    animation and movement done 12:30

--- src/js/index.js
+++ src/js/index.js
@@ -4,6 +4,7 @@
 console.log('it works!');
 const pacMan = document.querySelector('.entity--pac')
 
+
 let xpos = 0;
 let ypos = 0;
 const TILE_SIZE = 85;
@@ -15,14 +16,53 @@ class Pacman {
     this.yPos = yPos;
     this.mouth = mouth;
   }
-  moveRight() {
-    this.xPos++
+  // moveRight() {
+  //   this.xPos++
+  // }
+
+  // moveLeft() {
+  //   this.xPos--
+  // }
+
+  move() {
+    this.pacMan.classList.remove('pacman--right')
+    this.pacMan.classList.remove('pacman--left')
+    this.pacMan.classList.remove('pacman--down')
+    this.pacMan.classList.remove('pacman--up')
+    if (direction === 'right') {
+      this.xPos++
+      this.pacMan.classList.add('pacman--right')
+    } else if (direction === 'left') {
+      this.xPos--
+      this.pacMan.classList.add('pacman--left')
+    } else if (direction === 'down') {
+      this.yPos++
+      this.pacMan.classList.add('pacman--down')
+    } else if (direction === 'up') {
+      this.yPos--
+      this.pacMan.classList.add('pacman--up')
+    } else {
+      console.log('nothing')
+    }
   }
-  updateX() {
-    this.pacMan.style.left = this.xPos * TILE_SIZE + 'px';
+
+  update() {
+    this.pacMan.classList.toggle('pacman--closed')
+    if (direction === 'right' || direction === 'left') {
+      this.pacMan.style.left = this.xPos * TILE_SIZE + 'px';
+    } else if (direction === 'up' || direction === 'down') {
+      this.pacMan.style.top = this.yPos * TILE_SIZE + 'px';
+    } else {
+      console.log('notup')
+    }
     // this.mouth();
     // console.log(mouth);
   }
+
+  // mouth() {
+  //   this.pacman.classList.toggle('pacman--closed')
+  // }
+
   // mouth() { 
   //   if (this.mouth === true) { 
   //     this.mouth = false;
@@ -35,55 +75,52 @@ class Pacman {
 
 const pacman = new Pacman(0, 0, true, pacMan);
 
-// Toggle direction
 
+
+// Toggle direction
+let direction = 'right'
 // Right arrow
 document.addEventListener('keydown', (e) => {
   if (e.keyCode === 39) {
     console.log(e)
-    pacman.moveRight();
-    pacman.updateX();
-    pacMan.classList.add('pacman--right')
-    pacMan.classList.toggle('pacman--closed')
+    direction = 'right'
+    pacman.move();
+    pacman.update();
   } else {
-    pacMan.classList.remove('pacman--right')
+    console.log('no animation')
   }
 });
 // Left Arrow
 document.addEventListener('keydown', (e) => {
   if (e.keyCode === 37) {
     console.log(e)
-    xpos--
-    pacMan.style.left = (xpos * TILE_SIZE) + 'px';
-    pacMan.classList.add('pacman--left')
-    pacMan.classList.toggle('pacman--closed')
+    direction = 'left'
+    pacman.move();
+    pacman.update();
+    console.log(direction)
   } else {
-    pacMan.classList.remove('pacman--left')
+    console.log('no animation')
   }
 });
 
 //  down arrow
 document.addEventListener('keydown', (e) => {
   if (e.keyCode === 40) {
-    console.log(e)
-    ypos++
-    pacMan.style.top = (ypos * TILE_SIZE) + 'px';
-    pacMan.classList.add('pacman--down')
-    pacMan.classList.toggle('pacman--closed')
+    direction = 'down'
+    pacman.move();
+    pacman.update();
   } else {
-    pacMan.classList.remove('pacman--down')
+    console.log('no animation')
   }
 });
 
 // up arrow
 document.addEventListener('keydown', (e) => {
   if (e.keyCode === 38) {
-    console.log(e)
-    ypos--
-    pacMan.style.top = (ypos * TILE_SIZE) + 'px';
-    pacMan.classList.add('pacman--up')
-    pacMan.classList.toggle('pacman--closed')
+    direction = 'up'
+    pacman.move();
+    pacman.update();
   } else {
-    pacMan.classList.remove('pacman--up')
+    console.log('no animation')
   }
 });
