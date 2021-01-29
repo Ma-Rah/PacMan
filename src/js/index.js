82e921cf7e7dad56b56750cc39387b9ecce3594f
Ma-Rah <martin.lars.rahm@gmail.com>
2021-01-29 10:21:59 +0100

    Animation working 10:20

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,52 @@
 'use strict';
 
 console.log('it works!');
+const pacMan = document.querySelector('.entity--pac')
+
+
+// Toggle between open and closed mouth
+
+// Right arrow
+document.addEventListener('keydown', (e) => {
+  if (e.keyCode === 39) {
+    console.log(e)
+    pacMan.classList.add('pacman--right')
+    pacMan.classList.toggle('pacman--closed')
+  } else {
+    pacMan.classList.remove('pacman--right')
+  }
+});
+// Left Arrow
+document.addEventListener('keydown', (e) => {
+  if (e.keyCode === 37) {
+    console.log(e)
+    pacMan.classList.add('pacman--left')
+    pacMan.classList.toggle('pacman--closed')
+
+  } else {
+    pacMan.classList.remove('pacman--left')
+  }
+});
+
+//  down arrow
+document.addEventListener('keydown', (e) => {
+  if (e.keyCode === 40) {
+    console.log(e)
+    pacMan.classList.add('pacman--down')
+    pacMan.classList.toggle('pacman--closed')
+  } else {
+    pacMan.classList.remove('pacman--down')
+  }
+});
+
+// up arrow
+document.addEventListener('keydown', (e) => {
+  if (e.keyCode === 38) {
+    console.log(e)
+    pacMan.classList.add('pacman--up')
+    pacMan.classList.toggle('pacman--closed')
+
+  } else {
+    pacMan.classList.remove('pacman--up')
+  }
+});
